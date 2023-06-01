85edb05580fc6f2c721518f95d7b8c059d52ecdf
Nicolas KLEIN <nicolas6720@gmail.com>
2023-06-01 22:02:51 +0200

    removed .git in client subfolder, tracking client in the main repository

---
+++ react-client/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './scss/main.scss'
+import Header from './components/header';
+
+function App() {
+  return (
+    <div className="app">
+      <div>
+        <Header></Header>
+        Poker planning
+      </div>
+    </div>
+  );
+}
+
+export default App;
